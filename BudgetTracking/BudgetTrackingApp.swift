633dd457bc59ea0f7680f08b7966cbb603b494d2
Ama Ranasinghe <amaprabhashini926@gmail.com>
2023-09-27 00:28:38 +0530

    Initial Commit

---
+++ BudgetTracking/BudgetTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetTrackingApp.swift
+//  BudgetTracking
+//
+//  Created by Ama Ranasi on 2023-09-27.
+//
+
+import SwiftUI
+
+@main
+struct BudgetTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
