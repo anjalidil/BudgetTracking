f0f40832a28652d39bd2efb0ac3d73fafd46dc4f
anjalidil <anjalidilrangi@gmail.com>
2023-09-28 22:21:39 +0800

    Profile basic done

--- BudgetTracking/View/ProfileView.swift
+++ BudgetTracking/View/ProfileView.swift
@@ -6,10 +6,89 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
 struct ProfileView: View {
+    @AppStorage("uid") var userID: String = ""
+    @State private var userEmail: String? // Add a state property to hold the user's email
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        if userID == "" {
+            AuthView()
+        } else {
+            List {
+                Section {
+                    HStack {
+                        Image(systemName: "person")
+                            .imageScale(.large)
+                            .foregroundColor(Color(.white))
+                            .frame(width: 65, height: 65)
+                            .background(Color(.systemGray))
+                            .clipShape(Circle())
+                        
+                        VStack(alignment: .leading, spacing: 4) {
+                            Text(userEmail ?? "Unknown") // Display the user's email or "Unknown" if it's not available
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.top, 4)
+                        }
+                    }
+                }
+                
+                Section("Configurations") {
+                    Button {
+                        // Add your action here
+                    } label: {
+                        ProfileRowVIew(imageName: "list.clipboard.fill",
+                                       title: "Categories",
+                                       tintColor: .cyan)
+                    }
+                }
+                
+                Section("Account") {
+                    Button(action: {
+                        let firebaseAuth = Auth.auth()
+                        do {
+                            try firebaseAuth.signOut()
+                            withAnimation {
+                                userID = ""
+                            }
+                            
+                        } catch let signOutError as NSError {
+                            print("Error signing out: %@", signOutError)
+                        }
+                    }) {
+                        ProfileRowVIew(imageName: "power.circle.fill",
+                                       title: "Sign Out",
+                                       tintColor: .red)
+                    }
+                }
+                
+                Section("General") {
+                    HStack {
+                        ProfileRowVIew(imageName: "gear",
+                                       title: "Version",
+                                       tintColor: Color(.systemGray))
+                        Spacer()
+                        
+                        Text("1.0.0")
+                            .font(.subheadline)
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
+            .onAppear {
+                fetchUserEmail() // Fetch and update the user's email when the view appears
+            }
+        }
+    }
+    
+    // Function to fetch the user's email from Firebase Authentication
+    private func fetchUserEmail() {
+        if let user = Auth.auth().currentUser {
+            userEmail = user.email
+        }
     }
 }
 
