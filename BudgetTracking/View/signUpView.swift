01dd54bf905b401fee95e8423986fd7b819b8f70
KDatta97 <kaviddazun97@gmail.com>
2023-09-28 16:08:53 +0800

    change in signup

--- BudgetTracking/View/signUpView.swift
+++ BudgetTracking/View/signUpView.swift
@@ -1,26 +1,4 @@
-//
-//  signUpView.swift
-//  BudgetTracking
-//
-//  Created by Ama Ranasi on 2023-09-28.
-//
 
-//import SwiftUI
-//
-//struct signUpView: View {
-//    var body: some View {
-//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//    }
-//}
-//
-//struct signUpView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        signUpView()
-//    }
-//}
-
-
-//
 
 import SwiftUI
 import FirebaseAuth
