a143dce8567621f93f19319575f2bf7358d17293
anjalidil <anjalidilrangi@gmail.com>
2023-09-30 06:02:08 +0800

    partial created expenseView to load data.trying filters

--- BudgetTracking/View/ExpenseView.swift
+++ BudgetTracking/View/ExpenseView.swift
@@ -1,152 +1,174 @@
-//
-//  ExpenseView.swift
-//  BudgetTracking
-//
-//  Created by user244521 on 9/28/23.
-//
-
 import SwiftUI
 import Firebase
 import FirebaseFirestoreSwift
 
-struct Transactions: Identifiable, Hashable {
-    let id: String
-    let date: Date
-    let amount: Double
-    let type: String
-    let category: String
-    let remark: String
+struct ExpenseTransaction: Identifiable, Hashable, Decodable {
+    var id: String
+    var date: Date
+    var amount: Double
+    var type: String
+    var category: String
+    var detail: String
+    var userID: String
 }
 
 struct ExpenseView: View {
     
     @State private var isAddingNewExpense = false
     @AppStorage("uid") var userID: String = ""
-    @State private var transactions: [Transactions] = []
-//    @State private var selectedMonth = .currentMonth
+    @State private var transactions: [ExpenseTransaction] = []
     @State private var selectedTransactionType = 0
     let expenseTypeOptions = ["All", "Expense", "Income"]
     
+    // Date range selection
+    @State private var startDate: Date = Date() // Default to the current date
+        @State private var endDate: Date = Date()
     
-//    enum TransactionType: String, CaseIterable {
-//
-//        case expense = "Expense"
-//        case income = "Income"
-//        case all: "All"
-//    }
-    
-    
+    var filteredTransactions: [ExpenseTransaction] {
+           if selectedTransactionType == 0 {
+               // Show all transactions
+               return transactions.filter { transaction in
+                               transaction.date >= startDate && transaction.date <= endDate
+                           }
+           } else {
+               // Show filtered transactions based on the selected type
+               let selectedType = expenseTypeOptions[selectedTransactionType]
+               return transactions.filter { transaction in
+                   transaction.type == selectedType && transaction.date >= startDate && transaction.date <= endDate
+               }
+           }
+       }
     var body: some View {
-        
-        ZStack{
-            
+        NavigationView {
             VStack {
-                // Transaction Type Selector (Expenses/Income)
-                Picker("Select a Time Frame", selection: $selectedTransactionType) {
-                    ForEach(0..<expenseTypeOptions.count, id: \.self) { index in
-                        Text(expenseTypeOptions[index])
+                HStack{
+                    DatePicker("", selection: $startDate, in: ...Date(), displayedComponents: .date)
+                        .datePickerStyle(CompactDatePickerStyle())
+                        .padding(10)
+                        .font(.system(size: 12))
+                    
+                    DatePicker("to", selection: $endDate, in: ...Date(), displayedComponents: .date)
+                        .datePickerStyle(CompactDatePickerStyle())
+                        .padding(10)
+                        .font(.system(size: 12))
+                }
+                HStack {
+                    Picker("Select transaction type", selection: $selectedTransactionType) {
+                        ForEach(0..<expenseTypeOptions.count, id: \.self) { index in
+                            Text(expenseTypeOptions[index])
+                        }
                     }
-                }
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(10)
-                .pickerStyle(SegmentedPickerStyle())
-                .padding(10)
-                
-                
-                VStack(alignment: .leading, spacing: 5)
-                {
-//                    HStack {
-//                        if expenses.type == "Income" {
-//                            Image(systemName: "arrow.down.circle")
-//                                .foregroundColor(.green)
-//                        } else if expenses.type == "Expense" {
-//                            Image(systemName: "arrow.up.circle")
-//                                .foregroundColor(.red)
-//                        }
-//                    Text(" \(String(format: "%.2f", expenses.amount))")
-//                            .foregroundColor(.black)
-//                        Text(" \(expenses.category)")
-//                            .foregroundColor(.black)
-//                        Text(expenses.type)
-//                                .font(.headline)
-//                                .foregroundColor(textColorForType(expenses.type))
-//                    }
-//                    Text(": \(expenses.remark)")
-//                        .foregroundColor(.black)
-//
-//                    //  Spacer().frame(height: 50)
-//                    Text(" \(formattedDate(expenses.date))")
-//                        .foregroundColor(.black)
-//
-//
-//
+                    .pickerStyle(SegmentedPickerStyle())
+                    .padding(10)
+                    
+                    
                 }
                 
-                .padding(5)
-                .background(Color.white) // Set the view's background to black
-                .cornerRadius(10)
+                VStack(alignment: .leading, spacing: 20) {
+                    List(filteredTransactions) { transaction in
+                        HStack {
+                            VStack {
+                                HStack {
+                                    VStack {
+                                        Text(" \(transaction.category)")
+                                            .foregroundColor(.black)
+                                            .padding(0.6)
+                                            .font(.title3)
+                                        
+                                        Text(" \(transaction.detail)")
+                                            .foregroundColor(.black)
+                                            .font(.footnote)
+                                        Text(" \(formattedDate(transaction.date))")
+                                            .foregroundColor(.black.opacity(0.6))
+                                            .font(.footnote)
+                                    }
+                                    Spacer()
+                                    VStack {
+                                        HStack {
+                                            if transaction.type == "Income" {
+                                                Text("+")
+                                                    .foregroundColor(.green)
+                                            } else if transaction.type == "Expense" {
+                                                Text("-")
+                                                    .foregroundColor(.red)
+                                                    .padding(0.6)
+                                            }
+                                            
+                                            Text(" \(String(format: "%.2f", transaction.amount))")
+                                                .foregroundColor(textColorForType(transaction.type))
+                                                .font(.title3)
+                                        }
+                                        Text(" \(formattedDate(transaction.date))")
+                                            .foregroundColor(.black.opacity(0.6))
+                                            .font(.footnote)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    .padding(5)
+                    .background(Color.white)
+                    .cornerRadius(10)
+                }
+                
                 Button(action: {
                     isAddingNewExpense.toggle()
                 }) {
                     Image(systemName: "plus.circle.fill")
                         .resizable()
-                        .frame(width: 50, height: 50)
+                        .frame(width: 40, height: 40)
                         .padding()
-                        .background(Color.blue)
+                        .background(Color("bdcolor"))
                         .foregroundColor(.white)
                         .clipShape(Circle())
                         .padding(16)
                 }
-            }
-            .sheet(isPresented: $isAddingNewExpense) {
-                NavigationView {
-                    AddExpense()
-                        .navigationBarItems(
-                            leading: Button("Cancel") {
-                                isAddingNewExpense = false
-                            },
-                            trailing: Button("Save") {
-                                isAddingNewExpense = false
-                            }
-                        )
-                
-
-
-
-
-
-                    .sheet(isPresented: $isAddingNewExpense) {
-                                NavigationView {
-                                    AddExpense()
-                                        .navigationBarItems(
-                                            leading: Button("Cancel") {
-                                                isAddingNewExpense = false
-                                            },
-                                            trailing: Button("Save") {
-                                                isAddingNewExpense = false
-                                            }
-                                        )
+                .sheet(isPresented: $isAddingNewExpense) {
+                    NavigationView {
+                        AddExpense()
+                            .navigationBarItems(
+                                leading: Button("Cancel") {
+                                    isAddingNewExpense = false
                                 }
-                                
-                        }
+                            )
                     }
                 }
             }
-        
-        .ignoresSafeArea(.all)
-        
+            .onAppear {
+                fetchTransactions()
+            }
+            .navigationTitle("Expense Tracker")
+        }
     }
     
-    
+    func fetchTransactions() {
+        let db = Firestore.firestore()
+        var query = db.collection("Transaction").whereField("userID", isEqualTo: userID)
         
-
-
-
-    
-    
-   
-
-
+        query = query.whereField("date", isGreaterThanOrEqualTo: startDate)
+                          .whereField("date", isLessThanOrEqualTo: endDate)
+        
+        query.addSnapshotListener { (querySnapshot, error) in
+            if let error = error {
+                print("Error fetching transactions: \(error.localizedDescription)")
+            } else {
+                self.transactions = querySnapshot?.documents.compactMap { document in
+                    let data = document.data()
+                    if let amount = data["amount"] as? Double,
+                       let dateTimestamp = data["date"] as? Timestamp,
+                       let detail = data["detail"] as? String,
+                       let category = data["category"] as? String,
+                       let typeString = data["type"] as? String
+                    {
+                        let date = dateTimestamp.dateValue()
+                        return ExpenseTransaction(id: document.documentID, date: date, amount: amount, type: typeString, category: category, detail: detail, userID: self.userID)
+                    }
+                    return nil
+                } ?? []
+                print("Transactions loaded successfully: \(self.transactions)")
+            }
+        }
+    }
     
     func textColorForType(_ type: String) -> Color {
         if type == "Income" {
@@ -157,15 +179,10 @@ struct ExpenseView: View {
             return .primary
         }
     }
-
+    
     func formattedDate(_ date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "dd/mm/yyyy"
+        dateFormatter.dateFormat = "dd/MM/yyyy"
         return dateFormatter.string(from: date)
-    }
-}
-struct ExpenseView_Previews: PreviewProvider {
-    static var previews: some View {
-        ExpenseView()
     }
 }
